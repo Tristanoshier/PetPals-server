c7c7493b4bb545e68b85eee857b0cf6086b8abfc
Tristanoshier <tsoshier@bsu.edu>
2020-04-08 19:53:33 -0400

    get edit profile picture working

--- controllers/usercontroller.js
+++ controllers/usercontroller.js
@@ -1,8 +1,38 @@
+require('dotenv').config();
 const router = require('express').Router();
 const User = require('../db').import('../models/user');
 const bcrypt = require("bcryptjs");
 const jwt = require('jsonwebtoken');
 
+//IMAGE UPLOADING
+const aws = require('aws-sdk');
+const multerS3 = require('multer-s3');
+const multer = require('multer');
+const path = require('path');
+
+const s3 = new aws.S3
+
+//CONNECT TO AWS ACCOUNT
+aws.config.update({
+    accessKeyId: process.env.ACCESS_KEY,
+    secretAccessKey: process.env.SECRET_ACCESS_KEY,
+    region: 'us-east-2'
+});
+
+//CONNECTS TO AWS ACCOUNT AND MULTER TO UPLOAD SINGLE FILES
+const imgUpload = multer({
+    storage: multerS3({
+        s3: s3,
+        bucket: process.env.BUCKET_NAME,
+        acl: 'public-read',
+        metadata: function (req, file, cb) {
+          cb(null, { fieldName: file.fieldname });
+        },
+        key: function (req, file, cb) {
+            cb(null, path.basename(file.originalname, path.extname(file.originalname)) + '-' + Date.now() + path.extname(file.originalname))
+        }
+    })
+})
 
 //allows user to sign up for an account
 router.post('/signup', (req, res) => {
@@ -101,6 +131,19 @@ router.put("/update/:username", (req, res) => {
     })
         .then(profile => res.status(200).json(profile))
         .catch(err => res.json(req.errors));
+})
+
+router.put("/update/profile-pic/:username", imgUpload.single('image'), (req, res) => {
+    User.findOne({
+        where: {
+            username: req.params.username
+        }
+    }).then(user => {
+        user.update({
+            ProfileImg: req.file.location
+        })
+    }).then(profileImg => res.status(200).json(profileImg))
+    .catch(err => res.status(500).json(err))
 })
 
 
